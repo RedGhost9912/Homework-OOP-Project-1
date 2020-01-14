add7bf51c37d29d4751dc833f3e894a9c419776b
Deyan Atanasov <59844460+RedGhost9912@users.noreply.github.com>
2020-01-14 17:59:50 +0200

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,27 @@
+package library;
+
+import java.io.Serializable;
+
+public class Book implements Serializable {
+
+	private int isbn;
+	private String title, author;
+	
+	public Book(){
+		isbn = 0;
+		title = null;
+		author = null;		
+	}
+	
+	public Book(int isbn, String title, String author){
+		this.isbn = isbn;
+		this.title = title;
+		this.author = author;
+	}
+	
+	@Override
+	public String toString(){
+		return "\nTitle: " + title + "\nAuthor: " + author +
+				"\nISBN: " + isbn;
+	}
+}
---
+++ Main.java
@@ -0,0 +1,106 @@
+package library;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Scanner;
+
+public class Main {
+	static String fileName = null;
+	static Library lib = new Library();
+	static Scanner in = new Scanner(System.in);
+	static Boolean running = true;
+
+	public static void main(String[] args) {
+		while (running) {
+			System.out.println("\nEnter 0 for load a library."
+					+ "\nEnter 1 for save and quit"
+					+ "\nEnter 2 for list all books in library"
+					+ "\nEnter 3 for add book to library");
+
+			int answer = in.nextInt();
+			switch (answer) {
+			case 0:
+				System.out.println("Enter the file name to load");
+				loadScript(in.next());
+				break;
+
+			case 1:
+				saveAndQuit();
+				break;
+			case 2:
+				System.out.println(lib.toString());
+				break;
+			case 3:
+				addBook();
+				break;
+			}
+		}
+		System.exit(0);
+	}
+
+	private static void addBook() {
+		int isbn;
+		String title, author;		
+
+		System.out.println("\nEnter Title: ");
+		title = in.next();
+
+		System.out.println("\nEnter Author: ");
+		author = in.next();
+
+		System.out.println("\nEnter ISBN: ");
+		isbn = in.nextInt();
+		
+		Book b = new Book(isbn, title, author);
+		lib.addBook(b);
+	}
+
+	private static void saveAndQuit() {
+		System.out.println("Enter file name: ");
+		fileName = in.next() + ".ser";
+		running = false;
+		FileOutputStream fos = null;
+		ObjectOutputStream out = null;
+		try {
+			fos = new FileOutputStream(fileName);
+			out = new ObjectOutputStream(fos);
+			out.writeObject(lib);
+			fos.close();
+			out.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private static void loadScript(String name) {
+		FileInputStream fis = null;
+		ObjectInputStream in = null;
+		File file = new File(name + ".ser");
+		if (file.exists()) {
+			try {
+				fis = new FileInputStream(file);
+				in = new ObjectInputStream(fis);
+				lib = (Library) in.readObject();
+				fis.close();
+				in.close();
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+
+		} else {
+			System.out.println("\nThe file does not exist!");
+		}
+	}
+
+}
